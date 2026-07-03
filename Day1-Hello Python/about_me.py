315d9251efbe07ec30e5e66db868b54aa7f97404
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-03 20:13:14 +0530

    Day 1 - Hello Python

---
+++ Day1-Hello Python/about_me.py
@@ -0,0 +1,11 @@
+Name="Pooja"
+Age="28"
+Country="India"
+City="Mumbai"
+Profession="Teacher" 
+Dream_job="AI Intern"
+print("Name:",Name)
+print("Age:",Age)
+print("Country:",Country)
+print("City:",City)
+print("Profession:",Profession)
---
+++ Day2-Variables and datatypes/variables.py
@@ -0,0 +1,17 @@
+name = "Pooja"
+age = 28
+country = "India"
+city = "Bangkok"
+profession = "Content Creator"
+print(name)
+print(age,city)
+print(name)
+print(age)
+print(country)
+print(city)
+print(profession)
+print("Name:", name)
+print("Age:", age)
+print("Country:", country)
+print("City:", city)
+print("Profession:", profession)

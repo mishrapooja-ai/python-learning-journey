388d672c3d1c1a7149c353c2cbe84696ea3f0ef3
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-08 11:44:16 +0530

    Day-3 Python Operators

---
+++ Day 4- Operators/operators.py
@@ -0,0 +1,37 @@
+print("Arithmetic Operators")
+a=20
+b=5
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Modulus:",a%b)
+print("Division:",a/b)
+print("Exponent:",a**b)
+print("Floor Division:",a//b)
+
+print("\n Comparison Operators")
+x=10
+y=20
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x>=y)
+print(x<=y)
+
+
+print("\n Assignment Operators")
+number=10
+number+=5
+print(number)
+number-=3
+print(number)
+number*=3
+print(number)
+
+
+print("\n Logical Operators")
+age=22
+print(age>18 and age<30)
+print(age>18 or age>50)
+print(not(age>18))

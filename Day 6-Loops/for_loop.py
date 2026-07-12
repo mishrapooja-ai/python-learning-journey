354beaecfe2ee485557f06a82e8674c5154cbb88
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-12 10:56:19 +0530

    Day 6-Loops

---
+++ Day 6-Loops/for_loop.py
@@ -0,0 +1,11 @@
+print("\n Hello Python 5 times using for loop")
+for i in range(5):
+    print("Hello Python")
+
+print("\n My name 10 times") 
+name=input("Enter your name:")
+for i in range(10):
+    print(name)   
+
+
+
---
+++ Day 6-Loops/num1_100.py
@@ -0,0 +1,4 @@
+print("\n Print numbers 1-100")
+for i in range(1,101):
+    print(i)
+    

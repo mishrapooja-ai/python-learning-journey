0ad1bab78e890c8f9e0ff425db787db4db3a4409
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-16 15:54:06 +0530

    Day 8 - Lists

---
+++ Day 8-Lists/add_items.py
@@ -0,0 +1,10 @@
+#adding items in the list
+fruits=["Apple","Banana","Mango"]
+print(fruits)
+fruits.append("Orange")
+print(fruits)
+fruits.remove("Banana")
+print(fruits)
+
+#length of a list
+print("Number of Fruits:",len(fruits))
---
+++ Day 8-Lists/list_update.py
@@ -0,0 +1,4 @@
+fruits=["Apple","Banana","Mango"]
+print(fruits)
+fruits[1]="Orange"
+print(fruits)

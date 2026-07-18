a22ea9e011ae5d135422917394a2e17ec0f54637
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-18 16:20:04 +0530

    Day 8-List

---
+++ Day 8-Lists/student_marks.py
@@ -0,0 +1,23 @@
+#Student Marks Manager
+marks=[85,92,88,79,95]
+print("Student Marks:")
+print(marks)
+
+#Count The students
+
+print("Total Students:",len(marks))
+
+#Find Highest Mark
+print("The Highest Mark:",max(marks))
+
+#print Lowest Mark
+print("The Lowest mark:",min(marks))
+
+#Find Average
+average=sum(marks)/len(marks)
+print(average)
+
+print("All Students marks:")
+for mark in marks:
+    print(mark)
+
---
+++ Day 8-Lists/student_names.py
@@ -0,0 +1,8 @@
+names=["Ankita","Pooja","Muskan","Pretty","Rupa"]
+print("Student Names:",names)
+
+
+student_number=1
+for name in names:
+        print("Student" ,student_number ,":",name)
+        student_number+=1

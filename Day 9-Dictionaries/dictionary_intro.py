144c921af8ba1a86d439ff696e46279084958a1a
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-18 17:28:56 +0530

    Day 9-Dictionaries

---
+++ Day 9-Dictionaries/dictionary_intro.py
@@ -0,0 +1,13 @@
+#My first Dictionary
+student={
+    "name":"Pooja",
+    "age":"25",
+    "city":"Mumbai",
+    "dream_job":"AI Intern"
+
+}
+print(student)
+print(student["name"])
+print(student["age"])
+print(student["city"])
+print(student["dream_job"])

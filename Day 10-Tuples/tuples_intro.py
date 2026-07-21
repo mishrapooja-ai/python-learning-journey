303173e8c4bdf97f5447343c39b85eb729861c1f
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-21 11:15:34 +0530

    Day 10_Tuples

---
+++ Day 10-Tuples/tuples_intro.py
@@ -0,0 +1,10 @@
+fruits=("Apple","Mango","Banana","Grapes")
+print("\n My Fruits:")
+print(fruits)
+
+#print the Type
+student=("Pooja",26,"Python","AI Engineer")
+print(type(student))
+
+#Count The Items
+print(len(student))

26e93712baf889e10514629cc868faf4261cee7d
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-22 12:18:12 +0530

    Day 11-Sets

---
+++ Day 11-Sets/sets_intro.py
@@ -0,0 +1,20 @@
+#Order is not guaranteed in sets
+fruits={"Apple","Mango","Banana","Grapes"}
+print(fruits)
+
+#print only Unique names
+students={"Rahul","Pooja","Manik","Pooja","Rahul"}
+print(students)
+
+#print mixed types
+a={"Pooja","0.5",25,True}
+print(a)
+
+#creates empty set
+empty=set()
+print(type(empty))
+
+#Creating a set from list
+numbers=[10,20,30,40,50]
+unique_numbers=set(numbers)
+print(unique_numbers)

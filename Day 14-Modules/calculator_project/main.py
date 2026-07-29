13d4757aaeeacd1230cb6320f85c1701297d5a3c
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-07-29 15:04:23 +0530

    Day 14-Modules

---
+++ Day 14-Modules/calculator_project/main.py
@@ -0,0 +1,17 @@
+from addition import add
+answer =add(10,5)
+print(answer) 
+#subtraction
+from subtraction import subtract
+answer=subtract(10,20)
+print(answer)
+
+#multiplication
+from multiplication import multiply
+answer=multiply(10,20)
+print(answer)
+
+#division
+from division import divide
+answer=divide(10,20)
+print(answer)

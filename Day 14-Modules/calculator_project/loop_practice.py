7cda81b6c4ae9e9aa79591c88076a56d70c2be09
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-08-03 01:08:48 +0530

    Calculator_Project

---
+++ Day 14-Modules/calculator_project/loop_practice.py
@@ -0,0 +1,6 @@
+while True:
+    name=input("Enter your name (or type exit)")
+    if name.lower()=="exit":
+        print("GoodBye!")
+        break
+    print("Hello",name)
--- Day 14-Modules/calculator_project/main.py
+++ Day 14-Modules/calculator_project/main.py
@@ -1,17 +1,42 @@
+
 from addition import add
-answer =add(10,5)
-print(answer) 
-#subtraction
 from subtraction import subtract
-answer=subtract(10,20)
-print(answer)
-
-#multiplication
 from multiplication import multiply
-answer=multiply(10,20)
-print(answer)
-
-#division
 from division import divide
-answer=divide(10,20)
-print(answer)
+
+while True:
+    try:
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
+        print()
+        print("Choose Operation")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+         print("Answer =", add(num1, num2))
+
+        elif choice == "2":
+            print("Answer =", subtract(num1, num2))
+
+        elif choice == "3":
+            print("Answer =", multiply(num1, num2))
+
+        elif choice == "4":
+          print("Answer =", divide(num1, num2))
+
+        else:
+            print("Invalid Choice")
+        print()
+        print("Do you want another calculation?")
+        print("1. Yes")
+        print("2. Exit")
+
+        again = input("Enter your choice: ")
+        if again=="2":
+           print("Thankyou for using Calculator")
+           break
+    except ValueError:
+        print("Please enter only numbers.")

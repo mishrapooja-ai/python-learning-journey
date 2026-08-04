e9418240bc8859979d59a7a62da93dc084af9f04
Pooja Mishra <299404235+mishrapooja-ai@users.noreply.github.com>
2026-08-04 11:35:18 +0530

    OOP Practice

---
+++ OOP Practice/list_comprehension.py
@@ -0,0 +1,13 @@
+numbers=[1,2,3,4,5]
+squares=[num*num for num in numbers]
+print(squares)
+#multiiply every number by 5
+numbers=[2,3,4,6,8]
+result=[num*5 for num in numbers]
+print(result)
+
+#Convert every word to uppercase
+names=["python","pooja","ai","mumbai"]
+result=[name.upper() for name in names]
+print(result)
+
